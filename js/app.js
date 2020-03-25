8992e120a61398bb16f235c33ece5aaeebe9a46a
Blake Collins <blakeac369@gmail.com>
2020-03-24 20:51:11 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,62 +2,82 @@ const cards = [
     {
       front: 'The "First Computer Programmer?',
       back: 'Ada Lovelace',
-      flipped: false,
+      flipped: false
     },
     {
       front: 'Invented the "Clarke Calculator?',
       back: 'Edith Clarke',
-      flipped: false,
+      flipped: false
 
+    },
+    {
+      front: 'What is the use of blur function?',
+      back: 'Blur function is used to remove the focus from the specified object.',
+      flipped: false
     },
     {
       front: 'Famous World War II Enigma code breaker?',
       back: 'Alan Turing',
-      flipped: false,
+      flipped: false
     },
     {
       front: 'Created satellite orbit analyzation software for NASA?',
       back: 'Dr. Evelyn Boyd Granville',
-      flipped: false,
+      flipped: false
+    },
+    {
+      front: 'What are the different types of errors in JavaScript?',
+      back: 'Load time errors, run time errors, and logical errors',
+      flipped: false
     },
     {
       front:'Who invented the computer?',
       back:'Charles Babbage',
-      flipped: false,
+      flipped: false
     },
     {
       front:'When was JavaScript created?',
       back:'1995',
-      flipped: false,
+      flipped: false
     },
     {
       front:'What does HTML stand for?',
       back:'Hyper Text Markup Language',
-      flipped: false,
+      flipped: false
+    },
+    {
+      front: 'How are object properties assigned?',
+      back: 'obj["class"] = 12; or obj.class = 12;',
+      flipped: false
     },
     {
       front: 'What are JavaScript Data Types?',
       back:'Number, String, Boolean, Object, Undefined',
-      flipped: false,
+      flipped: false
     },
     {
       front:'What is the use of isNaN function?',
       back: 'isNan function returns true if the argument is not a number otherwise it is false.',
-      flipped: false,
+      flipped: false
     },
     {
       front: 'What are all the looping structures in JavaScript?',
       back: 'For, While, and do-while loops',
-      flipped: false,
+      flipped: false
     },
     {
       front: 'What are all the types of Pop up boxes available in JavaScript?',
       back:'Alert, Confirm, and Prompt',
-      flipped:false,
+      flipped:false
     },
     {
       front: 'What is the data type of variables of in JavaScript?',
       back: 'All variables in the JavaScript are object data types.',
+      flipped: false
+    },
+    {
+      front: 'What are the two basic groups of dataypes in JavaScript?',
+      back: 'Primitive and Reference types',
       flipped: false
     }
   ];
